f60f15260effe32bf42c4e2210536994012421d6
aayushsaajtak <aayush.sharma@aajtak.com>
2023-09-08 16:04:56 +0530

    added set group summary

--- clevertap-core/src/main/java/com/clevertap/android/sdk/pushnotification/CoreNotificationRenderer.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/pushnotification/CoreNotificationRenderer.java
@@ -130,8 +130,9 @@ public class CoreNotificationRenderer implements INotificationRenderer, AudibleN
                 .setAutoCancel(true)
                 .setSmallIcon(smallIcon)
                 .setShowWhen(false)
-                .setWhen(0L);
-//                .setGroup(grpKey);
+                .setWhen(0L)
+                .setGroup(grpKey)
+                .setGroupSummary(true);
 
         Log.i("ClevertapCustom", "Custom notification shown. Show when - false, group key" + grpKey);
 
