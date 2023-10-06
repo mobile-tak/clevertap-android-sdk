69040118cc8eff0b884cf1672fb6c892dd86dda6
aayushsaajtak <aayush.sharma@aajtak.com>
2023-10-06 11:52:38 +0530

    update

--- clevertap-core/src/main/java/com/clevertap/android/sdk/pushnotification/work/PushNotificationSchedulerWork.kt
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/pushnotification/work/PushNotificationSchedulerWork.kt
@@ -73,13 +73,13 @@ class PushNotificationSchedulerWork(context: Context, workerParams: WorkerParame
                 .setCustomHeadsUpContentView(contentView)
 
         // set priority low to disable heads up build and notify
-        nb.priority = NotificationCompat.PRIORITY_HIGH
+        nb.priority = NotificationCompat.PRIORITY_MAX
 
         try {
             val notificationId = (Math.random() * 100).toInt()
             val notif = nb.build()
 //            nm.cancel(notification.id)
-            nm.notify(notification.id, notif)
+            nm.notify(notification.id, n)
         } catch (e: Exception) {
             Logger.d(tag, "re trigger notification failed with error: $e")
         }
